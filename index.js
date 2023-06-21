b6adb0fdfee890f996e9457ad1aa95c1752008d1
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-06-21 03:29:37 -0400

    con tiempo

---
+++ index.js
@@ -0,0 +1,27 @@
+function dar() {
+    let todos = document.getElementsByTagName("td")
+    for (const este of todos) {
+        este.setAttribute("onclick", "marcar(this);")
+        este.removeAttribute("class")
+        este.classList.add("activo")
+        este.textContent = " "
+    }
+}
+
+const tiempo = ms => new Promise(res => setTimeout(res, ms));
+
+async function marcar(celda) {
+    cambiar(celda, "⭕")
+    let lista = document.getElementsByClassName("activo")
+    await tiempo(7400)
+    let elegido = lista[Math.floor(Math.random() * lista.length)]
+    cambiar(elegido, "❌")
+}
+
+function cambiar(celda, marca) {
+    celda.removeAttribute("onclick")
+    celda.textContent = marca
+    celda.classList.remove("activo")
+}
+
+dar()
